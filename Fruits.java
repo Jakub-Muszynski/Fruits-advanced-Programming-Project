45c61e7a5661bddefd386cf5954dfce3d5de57e5
Jakub Muszynski <71202201+Jakub-Muszynski@users.noreply.github.com>
2021-03-28 22:47:26 +0100

    Update Fruits.java

--- Fruits.java
+++ Fruits.java
@@ -1,1 +1,66 @@
+class Fruit{
+	//class Attributes
+	String Country_of_origin;
+	float Price_per_kilo;
+	float Weight
+
+	//constructor
+	public Fruit(string Country_of_origin, float Price_per_kilo, float weight{
+	this.County_of_origin = country_of_origin;
+		this.Price_per_kilo = Price_per_kilo;
+			this.Weight = Weight;
+
+
+}
+
+			//getter
+			public string getCountry_of_origin() {
+					return Country_of_origin;
+			}
+
+			//setter
+			Public void setCountry_of_origin(string newCountry_of_origin) {
+				this.Country_of_origin = newCountry_of_origin;
+
+				}
+
+				//getter
+				public float getPrice_per_kilo() {
+					return Price_per_kilo;
+
+
+					}
+
+				//setter
+				public void SetPrice_per_kilo(float newPrice_per_kilo) {
+					this.Price_per_kilo= newPrice_per_kilo;
+				}
+
+				//getter
+					public float getWeight() {
+						return Weight;
+
+					}
+				//setter
+				Public void setWeight(float newCountry_of_origin) {
+				this.Country_of_origin = newCountry_of_origin;
+
+
+
+
+
+
+
+class Apple extends Fruit{
+
+}
+
+
+public class fruitProject {
+
+    public static void main(String[] args) {
+
+
+    }
+}
 
