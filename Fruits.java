2eeb7c0a8072e7506afcb86312eb521cd12548d1
Jakub Muszynski <71202201+Jakub-Muszynski@users.noreply.github.com>
2021-05-12 23:23:26 +0100

    Update Fruits.java

--- Fruits.java
+++ Fruits.java
@@ -5,6 +5,7 @@ class Fruit{
 	float Price_per_kilo;
 	float Weight
 		
+		
 	//constructor
 	public Fruit(string Country_of_origin, float Price_per_kilo, float weight{
 	this.County_of_origin = country_of_origin;
